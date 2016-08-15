e2b27ec62c681b4d96c4ad3cbff16d25eac4a545
刀哥 <liufan321@gmail.com>
2016-08-15 16:03:31 +0800

    init project

---
+++ FFRuler/FFRuler/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FFRuler
+//
+//  Created by 刘凡 on 2016/8/15.
+//  Copyright © 2016年 joyios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FFRuler/FFRuler/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FFRuler
+//
+//  Created by 刘凡 on 2016/8/15.
+//  Copyright © 2016年 joyios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
